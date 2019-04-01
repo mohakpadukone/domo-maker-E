f2a646482a2b20fc5f76f5b8f6b7b245ae3eb08b
mohak <mvp2395@rit.edu>
2019-04-01 14:02:22 -0400

    commit

---
+++ client/app/maker.js
@@ -0,0 +1,99 @@
+const handleDomo = (e) =>{
+  e.preventDefault();
+  
+  $("#domoMessage").animate({width: 'hide'}, 350);
+  
+  if($("#domoName").val() == '' || $("#domoAge").val() == '' || $("#domoLives").val() == ''){//|| $("#domoLives").val() == ''){
+    handleError("RAWR! All fields are required");
+    return false;
+  }
+  
+  sendAjax('POST', $("#domoForm").attr("action"), $("#domoForm").serialize(), function(){
+    
+
+    const csrf = document.querySelector('#domoForm').querySelector('#csrfToken').value;
+    
+    loadDomosFromServer(csrf);
+  });
+  
+  return false;
+};
+
+const DomoForm = (props) =>{
+  return(
+  <form id="domoForm" name="domoForm"
+        onSubmit={handleDomo}
+        action="/maker"
+        method='POST'
+        className="domoForm"
+    >
+    <label htmlFor="name">Name: </label>
+    <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+    <label htmlFor="age">Age: </label>
+    <input id="domoAge" type="text" name="age" placeholder="Domo Age" />
+    <label htmlFor="lives">Lives: </label>
+    <input id="domoLives" type="text" name="lives" placeholder="Domo Lives" />
+    <input id="csrfToken" type="hidden" name="_csrf" value={props.csrf} />
+    <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+  </form>
+  );
+};
+
+const DomoList = function(props){
+  if(props.domos.length === 0){
+    return(
+      <div className="domoList">
+        <h3 className="emptyDomo">No Domos Yet</h3>
+      </div>
+    );
+  }
+  
+  const domoNodes = props.domos.map(function(domo){
+    return(
+      <div key={domo._id} className="domo">
+        <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+        <h3 className="domoName">Name: {domo.name}</h3>
+        <h3 className="domoAge">Age: {domo.age}</h3>     
+        <h3 className="domoLives">Lives: {domo.lives}</h3>   
+      </div>
+    );
+  });
+  
+  return(
+    <div className="domoList">
+      {domoNodes}
+    </div>
+  );
+};
+
+const loadDomosFromServer = (csrf) =>{
+  sendAjax('GET', '/getDomos', null, (data) =>{
+    ReactDOM.render(
+      <DomoList csrf={csrf} domos={data.domos} />,
+      document.querySelector("#domos")
+    );
+  });
+};
+
+const setup = function(csrf){
+  ReactDOM.render(
+    <DomoForm csrf={csrf} />,
+    document.querySelector("#makeDomo")
+  );
+  
+  ReactDOM.render(
+    <DomoList csrf={csrf} domos={[]} />,
+    document.querySelector("#domos")
+  ); 
+  loadDomosFromServer(csrf);
+};
+
+const getToken = () =>{
+  sendAjax('GET', '/getToken', null, (result) =>{
+    setup(result.csrfToken);
+  });
+};
+
+$(document).ready(function(){
+  getToken();
+});
---
+++ server/models/Domo.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const _ = require('underscore');
+
+let DomoModel = {};
+
+// mongoose.Types.ObjectiD is a func that
+// converts string isd to real mongo id
+const convertId = mongoose.Types.ObjectId;
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  lives: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+
+  createdData: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  age: doc.age,
+  lives: doc.lives,
+});
+
+DomoSchema.statics.findByOwner = (ownerId, callback) => {
+  const search = {
+    owner: convertId(ownerId),
+  };
+
+  return DomoModel.find(search).select('name age lives').exec(callback);
+};
+
+DomoModel = mongoose.model('Domo', DomoSchema);
+
+module.exports.DomoModel = DomoModel;
+module.exports.DomoSchema = DomoSchema;
